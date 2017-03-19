3158b5130f563b01d278561491b2f3374c955533
Jake Motta <motta_jake@Yahoo.com>
2017-03-18 17:17:30 -0700

    Initial Push
    Basic left/right walking, camera scrolling, and character animation

---
+++ hw2/src/Main.java
@@ -0,0 +1,180 @@
+import com.jogamp.nativewindow.WindowClosingProtocol;
+import com.jogamp.opengl.*;
+import com.jogamp.newt.event.KeyEvent;
+import com.jogamp.newt.event.KeyListener;
+import com.jogamp.newt.opengl.GLWindow;
+
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
+
+public class Main {
+	
+    // Set this to true to make the game loop exit.
+    private static boolean shouldExit;
+    
+    // Size of the sprite.
+    public static int[] spriteSize = new int[2];
+               
+    public static void main(String[] args) {
+          
+		// Game initialization goes here.
+    	
+    	Window window = new Window();
+    	GL2 gl = window.getGL();
+			
+        // The game loop
+        long lastFrameNS;
+        long curFrameNS = System.nanoTime();
+
+        Hero hero = new Hero(385, 284, spriteSize, gl);
+        Background background = new Background(spriteSize, gl);
+                
+        while (!shouldExit) {
+        	
+            System.arraycopy(window.kbState, 0, window.kbPrevState, 0, window.kbState.length);
+            lastFrameNS = curFrameNS;
+            curFrameNS = System.nanoTime();
+            long deltaTimeMS = (curFrameNS - lastFrameNS) / 1000000;
+            
+            long bgSpeed = (1 * deltaTimeMS)/4;
+            hero.setSpeed(bgSpeed);
+
+            // Actually, this runs the entire OS message pump.
+            window.myWindow.display();
+            
+            if (!window.myWindow.isVisible()) {
+                shouldExit = true;
+                break;
+            }
+            
+            hero.checkBackground(background);
+
+            // Game logic goes here.
+            if (window.kbState[KeyEvent.VK_ESCAPE]) {
+                shouldExit = true;
+            }
+
+            if (window.kbState[KeyEvent.VK_UP]) {   
+	        		//hero.keyDown("up");      		
+	        }
+	
+	        if (window.kbState[KeyEvent.VK_DOWN]) {
+	        		//hero.keyDown("down");
+	        }
+	        
+	
+	        if (window.kbState[KeyEvent.VK_LEFT]) {
+	        		hero.keyDown("left");      		
+	        }
+	        
+	        if (window.kbState[KeyEvent.VK_RIGHT]) {
+	        		hero.keyDown("right");
+	        }
+	         
+            window.gl.glClearColor(0, 0, 0, 1);
+            window.gl.glClear(GL2.GL_COLOR_BUFFER_BIT);
+            
+            background.draw(gl);
+            hero.update(gl);
+            
+        }
+    }
+
+    public static void glDrawSprite(GL2 gl, int tex, int x, int y, int w, int h) {
+        gl.glBindTexture(GL2.GL_TEXTURE_2D, tex);
+        gl.glBegin(GL2.GL_QUADS);
+        {
+            gl.glColor3ub((byte)-1, (byte)-1, (byte)-1);
+            gl.glTexCoord2f(0, 1);
+            gl.glVertex2i(x, y);
+            gl.glTexCoord2f(1, 1);
+            gl.glVertex2i(x + w, y);
+            gl.glTexCoord2f(1, 0);
+            gl.glVertex2i(x + w, y + h);
+            gl.glTexCoord2f(0, 0);
+            gl.glVertex2i(x, y + h);
+        }
+        gl.glEnd();
+    }
+    
+    
+    public static int glTexImageTGAFile(GL2 gl, String filename, int[] out_size) {
+        final int BPP = 4;
+
+        DataInputStream file = null;
+        try {
+            // Open the file.
+            file = new DataInputStream(new FileInputStream(filename));
+        } catch (FileNotFoundException ex) {
+            System.err.format("File: %s -- Could not open for reading.", filename);
+            return 0;
+        }
+
+        try {
+            // Skip first two bytes of data we don't need.
+            file.skipBytes(2);
+
+            // Read in the image type.  For our purposes the image type
+            // should be either a 2 or a 3.
+            int imageTypeCode = file.readByte();
+            if (imageTypeCode != 2 && imageTypeCode != 3) {
+                file.close();
+                System.err.format("File: %s -- Unsupported TGA type: %d", filename, imageTypeCode);
+                return 0;
+            }
+
+            // Skip 9 bytes of data we don't need.
+            file.skipBytes(9);
+
+            int imageWidth = Short.reverseBytes(file.readShort());
+            int imageHeight = Short.reverseBytes(file.readShort());
+            int bitCount = file.readByte();
+            file.skipBytes(1);
+
+            // Allocate space for the image data and read it in.
+            byte[] bytes = new byte[imageWidth * imageHeight * BPP];
+
+            // Read in data.
+            if (bitCount == 32) {
+                for (int it = 0; it < imageWidth * imageHeight; ++it) {
+                    bytes[it * BPP + 0] = file.readByte();
+                    bytes[it * BPP + 1] = file.readByte();
+                    bytes[it * BPP + 2] = file.readByte();
+                    bytes[it * BPP + 3] = file.readByte();
+                }
+            } else {
+                for (int it = 0; it < imageWidth * imageHeight; ++it) {
+                    bytes[it * BPP + 0] = file.readByte();
+                    bytes[it * BPP + 1] = file.readByte();
+                    bytes[it * BPP + 2] = file.readByte();
+                    bytes[it * BPP + 3] = -1;
+                }
+            }
+
+            file.close();
+
+            // Load into OpenGL
+            int[] texArray = new int[1];
+            gl.glGenTextures(1, texArray, 0);
+            int tex = texArray[0];
+            gl.glBindTexture(GL2.GL_TEXTURE_2D, tex);
+            gl.glTexImage2D(
+                    GL2.GL_TEXTURE_2D, 0, GL2.GL_RGBA, imageWidth, imageHeight, 0,
+                    GL2.GL_BGRA, GL2.GL_UNSIGNED_BYTE, ByteBuffer.wrap(bytes));
+            gl.glTexParameteri(GL2.GL_TEXTURE_2D, GL2.GL_TEXTURE_MIN_FILTER, GL2.GL_NEAREST);
+            gl.glTexParameteri(GL2.GL_TEXTURE_2D, GL2.GL_TEXTURE_MAG_FILTER, GL2.GL_NEAREST);
+
+            out_size[0] = imageWidth;
+            out_size[1] = imageHeight;
+            return tex;
+        }
+        catch (IOException ex) {
+            System.err.format("File: %s -- Unexpected end of file.", filename);
+            return 0;
+        }
+    }
+}
